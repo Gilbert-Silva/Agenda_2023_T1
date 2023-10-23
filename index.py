95f1c726c85a3aea87379ba39f2e723bcf616883
Gilbert-Silva <gilbertif1995@outlook.com>
2023-10-23 10:08:39 -0300

    Controle de Páginas atualizado

--- index.py
+++ index.py
@@ -2,29 +2,26 @@ from templates.inicioUI import InicioUI
 from templates.equacaoUI import EquacaoUI
 from templates.manterclienteUI import ManterClienteUI
 from templates.manterservicoUI import ManterServicoUI
+from templates.manteragendaUI import ManterAgendaUI
 import streamlit as st
 
 class IndexUI:
       
-    @classmethod
-    def sidebar(cls):
-      op = st.sidebar.selectbox("Menu", ["Equação", "Manter Clientes", "Manter Serviços"])
-      if op == "Equação": st.session_state["page"] = "equacaoUI"
-      if op == "Manter Clientes": st.session_state["page"] = "manter_clienteUI"
-      if op == "Manter Serviços": st.session_state["page"] = "manter_servicoUI"
+    def sidebar():
+      op = st.sidebar.selectbox("Menu", ["Manter Clientes", "Manter Serviços", "Manter Agenda"])
+      if op == "Manter Clientes": ManterClienteUI.main()
+      if op == "Manter Serviços": ManterServicoUI.main()
+      if op == "Manter Agenda": ManterAgendaUI.main()
+      #if op == "Manter Clientes": st.session_state["page"] = "manter_clienteUI"
+      #if op == "Manter Serviços": st.session_state["page"] = "manter_servicoUI"
+      #if op == "Manter Agenda": st.session_state["page"] = "manter_agendaUI"
 
-    @classmethod
-    def main(cls):
+    def main():
       IndexUI.sidebar()
-      if "page" not in st.session_state: st.session_state["page"] = "equacaoUI"
-      if st.session_state["page"] == "equacaoUI": EquacaoUI.main()
-      if st.session_state["page"] == "manter_clienteUI": ManterClienteUI.main()
-      if st.session_state["page"] == "manter_servicoUI": ManterServicoUI.main()
-
-      #InicioUI.main()
-      #EquacaoUI.main()
-      #ManterClienteUI.main()
-      #ManterServicoUI.main()
+      #if "page" not in st.session_state: st.session_state["page"] = "equacaoUI"
+      #if st.session_state["page"] == "manter_clienteUI": ManterClienteUI.main()
+      #if st.session_state["page"] == "manter_servicoUI": ManterServicoUI.main()
+      #if st.session_state["page"] == "manter_agendaUI": ManterAgendaUI.main()
 
 IndexUI.main()
 
